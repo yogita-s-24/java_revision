c37375b1da57c377d28139c16205c8a63f3941d4
yogitashete2412 <yogitashete24@gmail.com>
2023-03-01 12:37:50 +0530

    operators prog added

---
+++ myfirstproject/src/App2.java
@@ -0,0 +1,35 @@
+public class App2 {
+    public static void main( String args[]) throws Exception{
+        //Arithmetic operators
+        //     + - * /
+        int a = 10;
+        int b = 20;
+        int c;
+        c = a+b;
+        System.out.println("Addition is: "+c);
+
+        int p = 45;
+        int q = 10; 
+        int r;
+        r=p-q;
+        System.out.println("Subtraction is: "+r);
+
+        int x = 40;
+        int y = 10; 
+        int z;
+        z=x*y;
+        System.out.println("Multiplication is: "+z);
+
+        int e = 75;
+        int f = 25; 
+        int g;
+        g=e/f;
+        System.out.println("Division is: "+g);
+
+        int l = 495;
+        int m = 10; 
+        int n;
+        n=l%m;
+        System.out.println("Modulus is: "+n);
+    }
+}
