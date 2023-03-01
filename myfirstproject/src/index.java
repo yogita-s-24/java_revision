f520013f1a92c182ebb4d55be7c6385a3b2d8fd1
yogitashete2412 <yogitashete24@gmail.com>
2023-03-01 14:08:25 +0530

    get o/p from user

---
+++ myfirstproject/src/index.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class index {
+     public static void main(String[] args) {
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of A = ");
+        a=sc.nextInt();
+        System.out.println("Enter the value of B = ");
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println("Addition of c ="+c);
+        
+
+        
+    }
+}
